faeebebb5d72a878b6e3c0329be43358c4fab9d8
Jeremy Evans <code@jeremyevans.net>
2016-04-08 08:22:21 -0700

    Enable jwt feature on demo site

--- demo-site/rodauth_demo.rb
+++ demo-site/rodauth_demo.rb
@@ -17,21 +17,24 @@ class RodauthDemo < Roda
   SMS = {}
   MUTEX = Mutex.new
 
-  use Rack::Session::Cookie, :secret=>(ENV['SESSION_SECRET'] || SecureRandom.random_bytes(30)), :key => '_rodauth_demo_session'
+  secret = ENV['SESSION_SECRET'] || SecureRandom.random_bytes(30)
+  use Rack::Session::Cookie, :secret=>secret, :key => '_rodauth_demo_session'
   plugin :render, :escape=>true
   plugin :hooks
 
-  plugin :csrf
-  plugin :rodauth do
+  plugin :csrf, :skip_if => lambda{|req| req.env['CONTENT_TYPE'] =~ /application\/json/}
+  plugin :rodauth, :json=>true do
     enable :change_login, :change_password, :close_account, :create_account,
            :lockout, :login, :logout, :remember, :reset_password, :verify_account,
            :otp, :recovery_codes, :sms_codes, :password_complexity,
            :disallow_password_reuse, :password_expiration,
-           :account_expiration, :single_session
+           :account_expiration, :single_session, :jwt
     max_invalid_logins 2
     allow_password_change_after 60
     account_password_hash_column :ph
     title_instance_variable :@page_title
+    only_json? false
+    jwt_secret secret
     sms_send do |phone_number, message|
       MUTEX.synchronize{SMS[session_value] = "Would have sent the following SMS to #{phone_number}: #{message}"}
     end
