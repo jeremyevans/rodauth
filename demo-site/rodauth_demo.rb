21ab0327149ebd8f990aa260ee6deea0f8d70e67
Jeremy Evans <code@jeremyevans.net>
2016-04-11 12:23:26 -0700

    Use verify_change_login on demo site

--- demo-site/rodauth_demo.rb
+++ demo-site/rodauth_demo.rb
@@ -36,7 +36,7 @@ class RodauthDemo < Roda
            :otp, :recovery_codes, :sms_codes, :password_complexity,
            :disallow_password_reuse, :password_expiration, :password_grace_period,
            :account_expiration, :single_session, :jwt, :session_expiration,
-           :verify_account_grace_period
+           :verify_account_grace_period, :verify_change_login
     max_invalid_logins 2
     allow_password_change_after 60
     verify_account_grace_period 300
