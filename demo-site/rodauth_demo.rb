32e24bd0592fb3904fe4d15e3a3eee2041e09535
Jeremy Evans <code@jeremyevans.net>
2018-03-09 10:47:24 -0800

    Add Roda request_aref plugin to demo site

--- demo-site/rodauth_demo.rb
+++ demo-site/rodauth_demo.rb
@@ -28,6 +28,7 @@ class App < Roda
   secret = ENV.delete('RODAUTH_SESSION_SECRET') || ENV.delete('SESSION_SECRET') || SecureRandom.random_bytes(30)
   use Rack::Session::Cookie, :secret=>secret, :key => '_rodauth_demo_session'
   plugin :render, :escape=>true
+  plugin :request_aref, :raise
   plugin :hooks
 
   plugin :csrf, :skip_if => lambda{|req| req.env['CONTENT_TYPE'] =~ /application\/json/}
