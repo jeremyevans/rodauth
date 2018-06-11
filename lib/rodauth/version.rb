5b366de4a8527000c1b3cb80c0354c8e75bc556c
Jeremy Evans <code@jeremyevans.net>
2018-06-11 10:54:41 -0700

    Add MAJOR/MINOR/TINY/VERSION_NUMBER constants with version information

--- lib/rodauth/version.rb
+++ lib/rodauth/version.rb
@@ -1,7 +1,22 @@
 # frozen-string-literal: true
 
 module Rodauth
-  VERSION = '1.16.0'.freeze
+  # The major version of Rodauth, updated only for major changes that are
+  # likely to require modification to apps using Rodauth.
+  MAJOR = 1
+
+  # The minor version of Rodauth, updated for new feature releases of Rodauth.
+  MINOR = 16
+
+  # The patch version of Rodauth, updated only for bug fixes from the last
+  # feature release.
+  TINY = 0
+
+  # The full version of Rodauth as a string
+  VERSION = "#{MAJOR}.#{MINOR}.#{TINY}".freeze
+
+  # The full version of Rodauth as a number (1.17.0 => 11700)
+  VERSION_NUMBER = MAJOR*10000 + MINOR*100 + TINY
 
   def self.version
     VERSION
