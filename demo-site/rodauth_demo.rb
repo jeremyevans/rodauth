3775425b2ffe3653ad204aa32be0c395a0526639
Jeremy Evans <code@jeremyevans.net>
2018-07-03 12:02:29 -0700

    Make demo site use Roda route_csrf and sessions plugins

--- demo-site/rodauth_demo.rb
+++ demo-site/rodauth_demo.rb
@@ -25,14 +25,16 @@ class App < Roda
   SMS = {}
   MUTEX = Mutex.new
 
-  secret = ENV.delete('RODAUTH_SESSION_SECRET') || ENV.delete('SESSION_SECRET') || SecureRandom.random_bytes(30)
-  use Rack::Session::Cookie, :secret=>secret, :key => '_rodauth_demo_session'
   plugin :render, :escape=>true
   plugin :request_aref, :raise
   plugin :hooks
+  plugin :flash
 
-  plugin :csrf, :skip_if => lambda{|req| req.env['CONTENT_TYPE'] =~ /application\/json/}
-  plugin :rodauth, :json=>true do
+  cipher_secret = ENV.delete('RODAUTH_SESSION_CIPHER_SECRET') || SecureRandom.random_bytes(32)
+  hmac_secret = ENV.delete('RODAUTH_SESSION_HMAC_SECRET') || SecureRandom.random_bytes(32)
+  plugin :sessions, :cipher_secret=>cipher_secret, :hmac_secret=>hmac_secret, :key=>'rodauth-demo.session'
+
+  plugin :rodauth, :json=>true, :csrf=>:route_csrf do
     db DB
     enable :change_login, :change_password, :close_account, :create_account,
            :lockout, :login, :logout, :remember, :reset_password, :verify_account,
@@ -48,7 +50,7 @@ class App < Roda
     title_instance_variable :@page_title
     only_json? false
     json_response_custom_error_status? true
-    jwt_secret secret
+    jwt_secret(cipher_secret+hmac_secret)
     sms_send do |phone_number, message|
       MUTEX.synchronize{SMS[session_value] = "Would have sent the following SMS to #{phone_number}: #{message}"}
     end
@@ -70,6 +72,7 @@ class App < Roda
   end
 
   route do |r|
+    check_csrf! unless r.env['CONTENT_TYPE'] =~ /application\/json/
     rodauth.load_memory
     rodauth.check_session_expiration
     rodauth.update_last_activity
