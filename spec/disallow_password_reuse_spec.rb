e16b69eb24cdfa1cecedfbc16bd44081fbb99128
Jeremy Evans <code@jeremyevans.net>
2019-05-03 16:39:39 -0700

    Add specs for disallow_password_reuse fix

--- spec/disallow_password_reuse_spec.rb
+++ spec/disallow_password_reuse_spec.rb
@@ -54,39 +54,126 @@ describe 'Rodauth disallow_password_reuse feature' do
     DB[table].get{count(:id)}.must_equal 0
   end
 
-  it "should handle create account when account_password_hash_column is true" do
-    rodauth do
-      enable :login, :create_account, :change_password, :disallow_password_reuse
-      if ENV['RODAUTH_SEPARATE_SCHEMA']
-        previous_password_hash_table Sequel[:rodauth_test_password][:account_previous_password_hashes]
+  [true, false].each do |ph|
+    it "should handle create account when account_password_hash_column is #{ph}" do
+      rodauth do
+        enable :login, :create_account, :change_password, :disallow_password_reuse
+        if ENV['RODAUTH_SEPARATE_SCHEMA']
+          previous_password_hash_table Sequel[:rodauth_test_password][:account_previous_password_hashes]
+        end
+        account_password_hash_column :ph if ph
+        change_password_requires_password? false
       end
-      account_password_hash_column :ph
-      change_password_requires_password? false
+      roda do |r|
+        r.rodauth
+        r.root{view :content=>""}
+      end
+
+      visit '/create-account'
+      fill_in 'Login', :with=>'bar@example.com'
+      fill_in 'Confirm Login', :with=>'bar@example.com'
+      fill_in 'Password', :with=>'0123456789'
+      fill_in 'Confirm Password', :with=>'0123456789'
+      click_button 'Create Account'
+      page.current_path.must_equal '/'
+      page.find('#notice_flash').text.must_equal "Your account has been created"
+
+      visit '/change-password'
+      fill_in 'New Password', :with=>"012345678"
+      fill_in 'Confirm Password', :with=>"012345678"
+      click_button 'Change Password'
+      page.find('#notice_flash').text.must_equal "Your password has been changed"
+
+      visit '/change-password'
+      fill_in 'New Password', :with=>"0123456789"
+      fill_in 'Confirm Password', :with=>"0123456789"
+      click_button 'Change Password'
+      page.html.must_include("invalid password, does not meet requirements (same as previous password)")
     end
-    roda do |r|
-      r.rodauth
-      r.root{view :content=>""}
+
+    it "should handle verify account when account_password_hash_column is #{ph}" do
+      rodauth do
+        enable :login, :verify_account, :change_password, :disallow_password_reuse
+        if ENV['RODAUTH_SEPARATE_SCHEMA']
+          previous_password_hash_table Sequel[:rodauth_test_password][:account_previous_password_hashes]
+        end
+        account_password_hash_column :ph if ph
+        change_password_requires_password? false
+      end
+      roda do |r|
+        r.rodauth
+        r.root{view :content=>""}
+      end
+
+      visit '/create-account'
+      fill_in 'Login', :with=>'bar@example.com'
+      fill_in 'Confirm Login', :with=>'bar@example.com'
+      fill_in 'Password', :with=>'0123456789'
+      fill_in 'Confirm Password', :with=>'0123456789'
+      click_button 'Create Account'
+      page.current_path.must_equal '/'
+      page.find('#notice_flash').text.must_equal "An email has been sent to you with a link to verify your account"
+      link = email_link(/(\/verify-account\?key=.+)$/, 'bar@example.com')
+
+      visit link
+      click_button 'Verify Account'
+      page.find('#notice_flash').text.must_equal "Your account has been verified"
+      page.current_path.must_equal '/'
+
+      visit '/change-password'
+      fill_in 'New Password', :with=>"012345678"
+      fill_in 'Confirm Password', :with=>"012345678"
+      click_button 'Change Password'
+      page.find('#notice_flash').text.must_equal "Your password has been changed"
+
+      visit '/change-password'
+      fill_in 'New Password', :with=>"0123456789"
+      fill_in 'Confirm Password', :with=>"0123456789"
+      click_button 'Change Password'
+      page.html.must_include("invalid password, does not meet requirements (same as previous password)")
     end
 
-    visit '/create-account'
-    fill_in 'Login', :with=>'bar@example.com'
-    fill_in 'Confirm Login', :with=>'bar@example.com'
-    fill_in 'Password', :with=>'0123456789'
-    fill_in 'Confirm Password', :with=>'0123456789'
-    click_button 'Create Account'
-    page.current_path.must_equal '/'
-    page.find('#notice_flash').text.must_equal "Your account has been created"
+    it "should handle verify account when account_password_hash_column is #{ph} and verify_account_set_password? is true" do
+      rodauth do
+        enable :login, :verify_account, :change_password, :disallow_password_reuse
+        if ENV['RODAUTH_SEPARATE_SCHEMA']
+          previous_password_hash_table Sequel[:rodauth_test_password][:account_previous_password_hashes]
+        end
+        account_password_hash_column :ph if ph
+        change_password_requires_password? false
+        verify_account_set_password? true
+      end
+      roda do |r|
+        r.rodauth
+        r.root{view :content=>""}
+      end
 
-    visit '/change-password'
-    fill_in 'New Password', :with=>"012345678"
-    fill_in 'Confirm Password', :with=>"012345678"
-    click_button 'Change Password'
-    page.find('#notice_flash').text.must_equal "Your password has been changed"
+      visit '/create-account'
+      fill_in 'Login', :with=>'bar@example.com'
+      fill_in 'Confirm Login', :with=>'bar@example.com'
+      click_button 'Create Account'
+      page.current_path.must_equal '/'
+      page.find('#notice_flash').text.must_equal "An email has been sent to you with a link to verify your account"
+      link = email_link(/(\/verify-account\?key=.+)$/, 'bar@example.com')
 
-    visit '/change-password'
-    fill_in 'New Password', :with=>"0123456789"
-    fill_in 'Confirm Password', :with=>"0123456789"
-    click_button 'Change Password'
-    page.html.must_include("invalid password, does not meet requirements (same as previous password)")
+      visit link
+      fill_in 'Password', :with=>'0123456789'
+      fill_in 'Confirm Password', :with=>'0123456789'
+      click_button 'Verify Account'
+      page.find('#notice_flash').text.must_equal "Your account has been verified"
+      page.current_path.must_equal '/'
+
+      visit '/change-password'
+      fill_in 'New Password', :with=>"012345678"
+      fill_in 'Confirm Password', :with=>"012345678"
+      click_button 'Change Password'
+      page.find('#notice_flash').text.must_equal "Your password has been changed"
+
+      visit '/change-password'
+      fill_in 'New Password', :with=>"0123456789"
+      fill_in 'Confirm Password', :with=>"0123456789"
+      click_button 'Change Password'
+      page.html.must_include("invalid password, does not meet requirements (same as previous password)")
+    end
   end
 end
