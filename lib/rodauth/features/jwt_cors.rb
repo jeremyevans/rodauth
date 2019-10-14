50a79c5ff5e7bc1bd59762bbe8cea5d42d092172
Jeremy Evans <code@jeremyevans.net>
2019-10-14 14:12:22 -0700

    Add jwt_cors feature to handle Cross-Origin Resource Sharing when using the jwt feature

---
+++ lib/rodauth/features/jwt_cors.rb
@@ -0,0 +1,53 @@
+# frozen-string-literal: true
+
+module Rodauth
+  Feature.define(:jwt_cors, :JwtCors) do
+    depends :jwt
+
+    auth_value_method :jwt_cors_allow_origin, false
+    auth_value_method :jwt_cors_allow_methods, 'POST'
+    auth_value_method :jwt_cors_allow_headers, 'Content-Type, Authorization, Accept'
+    auth_value_method :jwt_cors_expose_headers, 'Authorization'
+    auth_value_method :jwt_cors_max_age, 86400
+
+    auth_methods(:jwt_cors_allow?)
+
+    def jwt_cors_allow?
+      if origin = request.env['HTTP_ORIGIN']
+        case allowed = jwt_cors_allow_origin
+        when String
+          timing_safe_eql?(origin, allowed)
+        when Array
+          allowed.any?{|s| timing_safe_eql?(origin, s)}
+        when Regexp
+          allowed =~ origin
+        when true
+          true
+        else
+          false
+        end
+      end
+    end
+
+    private
+
+    def before_rodauth
+      if (origin = request.env['HTTP_ORIGIN']) && jwt_cors_allow?
+        response['Access-Control-Allow-Origin'] = origin
+
+        # Handle CORS preflight request
+        if request.request_method == 'OPTIONS'
+          response['Access-Control-Allow-Methods'] = jwt_cors_allow_methods
+          response['Access-Control-Allow-Headers'] = jwt_cors_allow_headers
+          response['Access-Control-Max-Age'] = jwt_cors_max_age.to_s
+          response.status = 204
+          request.halt(response.finish)
+        end
+
+        response['Access-Control-Expose-Headers'] = jwt_cors_expose_headers
+      end
+
+      super
+    end
+  end
+end
---
+++ spec/jwt_cors_spec.rb
@@ -0,0 +1,57 @@
+require File.expand_path("spec_helper", File.dirname(__FILE__))
+
+describe 'Rodauth jwt_cors feature' do
+  it "should support CORS logins if allowed" do
+    origin = false
+    rodauth do
+      enable :login, :jwt_cors
+      jwt_secret '1'
+      json_response_success_key 'success'
+      jwt_cors_allow_origin{origin}
+    end
+    roda(:csrf=>false, :json=>true) do |r|
+      r.rodauth
+      rodauth.require_authentication
+      response['Content-Type'] = 'application/json'
+      '1'
+    end
+
+    # CORS Preflight Request
+    preflight_request = {
+      :method=>'OPTIONS',
+      :headers=>{
+        "HTTP_ACCESS_CONTROL_REQUEST_METHOD"=>"POST",
+        "HTTP_ORIGIN"=>"https://foo.example.com",
+        "HTTP_ACCESS_CONTROL_REQUEST_HEADERS"=>"content-type",
+        "CONTENT_TYPE"=>' application/json'
+      }
+    }
+
+    res = json_request("/login", preflight_request.dup)
+    res.must_equal [405, ["{\"error\":\"non-POST method used in JSON API\"}"]]
+
+    origin = Object.new
+    res = json_request("/login", preflight_request.dup)
+    res.must_equal [405, ["{\"error\":\"non-POST method used in JSON API\"}"]]
+
+    ["https://foo.example.com", ["https://foo.example.com"], %r{https://foo.example.com}, true].each do |orig|
+      origin = orig
+
+      res = json_request("/login", preflight_request.merge(:include_headers=>true))
+      res[0].must_equal 204
+      res[1]['Access-Control-Allow-Origin'].must_equal "https://foo.example.com"
+      res[1]['Access-Control-Allow-Methods'].must_equal "POST"
+      res[1]['Access-Control-Allow-Headers'].must_equal "Content-Type, Authorization, Accept"
+      res[1]['Access-Control-Max-Age'].must_equal "86400"
+      res[2].must_equal []
+
+      res = json_request("/login", :login=>'foo@example.com', :password=>'0123456789', :headers=>{"HTTP_ORIGIN"=>"https://foo.example.com"}, :include_headers=>true)
+      res[0].must_equal 200
+      res[1]['Access-Control-Allow-Origin'].must_equal "https://foo.example.com"
+      res[1]['Access-Control-Expose-Headers'].must_equal "Authorization"
+      res[2].must_equal("success"=>"You have been logged in")
+
+      json_request("/foo").must_equal [200, 1]
+    end
+  end
+end
