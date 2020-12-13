cacbdbc7d0134d5906c1e097204685061b7f8b3c
Jeremy Evans <code@jeremyevans.net>
2020-12-13 15:15:51 -0800

    Avoid verbose mode warnings in make_www.rb

--- www/make_www.rb
+++ www/make_www.rb
@@ -5,7 +5,7 @@ Dir.chdir(File.dirname(__FILE__))
 erb = ERB.new(File.read('layout.erb'))
 Dir['pages/*.erb'].each do |page|
   public_loc = "#{page.gsub(/\Apages\//, 'public/').sub('.erb', '.html')}"
-  content = ERB.new(File.read(page)).result(binding)
-  title = File.basename(page.sub('.erb', ''))
+  content = content = ERB.new(File.read(page)).result(binding)
+  title = title = File.basename(page.sub('.erb', ''))
   File.open(public_loc, 'wb'){|f| f.write(erb.result(binding))}
 end
