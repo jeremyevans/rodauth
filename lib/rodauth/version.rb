ffdffddf3d7dbe973fb6208d5f973587cd69afc0
Jeremy Evans <code@jeremyevans.net>
2021-04-22 07:54:25 -0700

    Bump version to 2.12.0

--- lib/rodauth/version.rb
+++ lib/rodauth/version.rb
@@ -6,7 +6,7 @@ module Rodauth
   MAJOR = 2
 
   # The minor version of Rodauth, updated for new feature releases of Rodauth.
-  MINOR = 11
+  MINOR = 12
 
   # The patch version of Rodauth, updated only for bug fixes from the last
   # feature release.
