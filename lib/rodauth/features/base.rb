5289e1ca494cd6a8bade4882decca9fd01e00fbe
Bo Jeanes <me@bjeanes.com>
2022-03-04 11:22:02 +1100

    Fix bug with update_hash_ds

--- lib/rodauth/features/base.rb
+++ lib/rodauth/features/base.rb
@@ -756,7 +756,7 @@ module Rodauth
       num = ds.update(values)
       if num == 1
         values.each do |k, v|
-          account[k] = Sequel::CURRENT_TIMESTAMP == v ? Time.now : v
+          hash[k] = Sequel::CURRENT_TIMESTAMP == v ? Time.now : v
         end
       end
       num
