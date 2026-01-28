1df3182fedf510c181b57a649d375a068290d35a
Mike Manfrin <manfrin@gmail.com>
2026-01-28 10:37:08 -0800

    Adding tests for the implicit verification of accounts that have their password successfully reset.

--- spec/reset_password_spec.rb
+++ spec/reset_password_spec.rb
@@ -226,6 +226,37 @@ describe 'Rodauth reset_password feature' do
     page.current_path.must_equal '/reset-password-request'
   end
 
+  it "should support implicit verification when resetting passwords for unverified accounts" do
+    rodauth do
+      enable :login, :reset_password, :verify_account
+      reset_password_implicitly_verifies? true
+      reset_password_autologin? true
+    end
+    roda do |r|
+      r.rodauth
+      r.root{view :content=>rodauth.logged_in? ? "Logged In" : "Not Logged"}
+    end
+
+    DB[:accounts].update(:status_id=>1)
+    DB[:account_verification_keys].insert(:id=>DB[:accounts].get(:id), :key=>'test')
+
+    visit '/reset-password-request'
+    fill_in 'Login', :with=>'foo@example.com'
+    click_button 'Request Password Reset'
+    page.find('#notice_flash').text.must_equal "An email has been sent to you with a link to reset the password for your account"
+
+    link = email_link(/(\/reset-password\?key=.+)$/)
+    visit link
+    fill_in 'Password', :with=>'0123456'
+    fill_in 'Confirm Password', :with=>'0123456'
+    click_button 'Reset Password'
+    page.find('#notice_flash').text.must_equal "Your password has been reset"
+    page.body.must_include("Logged In")
+
+    DB[:accounts].get(:status_id).must_equal 2
+    DB[:account_verification_keys].count.must_equal 0
+  end
+
   [true, false].each do |before|
     it "should clear reset password token when closing account, when loading reset_password #{before ? "before" : "after"}" do
       rodauth do
